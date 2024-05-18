06ff231d76d2d770e6d3fed9341efe4e3d0261be
Miqueias Pires <miqueiasousa011@gmail.com>
2024-05-18 14:44:29 -0300

    primeiro commit

---
+++ src/main/java/br/com/roupaspet/RoupaspetApplication.java
@@ -0,0 +1,13 @@
+package br.com.roupaspet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoupaspetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoupaspetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/roupaspet/RoupaspetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.roupaspet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoupaspetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
